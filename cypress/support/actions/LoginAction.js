5a88b7e3b30c027be86a6ca4206a26a53542e529
Nina Sitaeva <sitaeva.nn@gmail.com>
2023-04-09 02:28:11 +0200

    chore: update test running script

--- cypress/support/actions/LoginAction.js
+++ cypress/support/actions/LoginAction.js
@@ -8,9 +8,10 @@ class LoginAction {
     loginRepo.getEmailField().clear().type(email, { delay: 30 });
     loginRepo.getPasswordField().clear().type(password, { delay: 30 });
     // added delay, as too fast requests are blocked
-    cy.wait(2000);
+    cy.wait(3000);
     loginRepo.getConfirmLoginBtn().click();
 
+    // TODO: implement check whether digit code was sent and provide this code
     cy.pause();
 
     // QA should provide SMS-code if asked by a system.
